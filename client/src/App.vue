8642c01b432f79953a7c4c7a66921b1320fe7f91
Caruychen <caruychen@gmail.com>
2020-10-06 17:41:08 +0300

    add navigation component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TheNavigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import TheNavigation from "@/components/TheNavigation.vue"
+export default {
+  components: {
+    TheNavigation
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
