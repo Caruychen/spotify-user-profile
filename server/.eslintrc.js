0bb7413cc67c6e475adb31b7ad1b11af32644e58
Caruychen <caruychen@gmail.com>
2020-10-06 12:39:05 +0300

    configure eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": ["eslint:recommended","plugin:prettier/recommended"],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error"
+    }
+};
