89614c567df7480bfd7d06b0d7a75825971b1eb9
Caruychen <caruychen@gmail.com>
2020-10-07 15:38:45 +0300

    add sass

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,20 @@
 <template>
   <div id="app">
-    <TheNavigation />
     <router-view />
   </div>
 </template>
 
 <script>
-import TheNavigation from "@/components/TheNavigation.vue"
 export default {
-  components: {
-    TheNavigation
-  }
-}
+  components: {}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/sass/index.sass";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
