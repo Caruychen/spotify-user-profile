1cf141fcf568df5f489470d174a09f978b1f6763
Caruychen <caruychen@gmail.com>
2020-10-07 15:39:28 +0300

    update navigation

--- client/src/components/TheNavigation.vue
+++ client/src/components/TheNavigation.vue
@@ -1,7 +1,41 @@
 <template>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="nav">
+    <b-nav vertical>
+      <b-nav-item to="/user">
+        <b-icon-house></b-icon-house>
+        <br />
+        Home
+      </b-nav-item>
+      <b-nav-item to="/artists">
+        <b-icon-star></b-icon-star>
+        <br />
+        Top Artists
+      </b-nav-item>
+      <b-nav-item to="/tracks">
+        <b-icon-music-note-beamed></b-icon-music-note-beamed>
+        <br />
+        Top Tracks
+      </b-nav-item>
+      <b-nav-item to="/recent">
+        <b-icon-clock-history></b-icon-clock-history>
+        <br />
+        Recent
+      </b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
+<style scoped>
+#nav {
+  position: fixed;
+  min-height: 100vh;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#nav a {
+  padding: 20px;
+}
+</style>
