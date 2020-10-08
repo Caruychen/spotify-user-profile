49002a68601a09bbace0705231dcc292aa716ee9
Caruychen <caruychen@gmail.com>
2020-10-08 09:59:49 +0300

    axios update

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
@@ -8,8 +10,23 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/sass/index.sass";
 
+Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+axios.interceptors.request.use(
+  config => {
+    if (localStorage.access_token) {
+      config.headers = {
+        Authorization: "Bearer " + localStorage.access_token
+      };
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 Vue.config.productionTip = false;
 
