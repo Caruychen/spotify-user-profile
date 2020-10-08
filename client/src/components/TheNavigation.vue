b80e7d692299157e9c207c3f0adaad6659e3496c
Caruychen <caruychen@gmail.com>
2020-10-08 10:00:30 +0300

    add responsive nav style

--- client/src/components/TheNavigation.vue
+++ client/src/components/TheNavigation.vue
@@ -1,41 +1,24 @@
 <template>
-  <div id="nav">
-    <b-nav vertical>
-      <b-nav-item to="/user">
-        <b-icon-house></b-icon-house>
-        <br />
-        Home
-      </b-nav-item>
-      <b-nav-item to="/artists">
-        <b-icon-star></b-icon-star>
-        <br />
-        Top Artists
-      </b-nav-item>
-      <b-nav-item to="/tracks">
-        <b-icon-music-note-beamed></b-icon-music-note-beamed>
-        <br />
-        Top Tracks
-      </b-nav-item>
-      <b-nav-item to="/recent">
-        <b-icon-clock-history></b-icon-clock-history>
-        <br />
-        Recent
-      </b-nav-item>
-    </b-nav>
-  </div>
+  <nav class="nav" id="nav">
+    <router-link class="nav-link" to="/user">
+      <b-icon-house></b-icon-house>
+      <br />
+      Home
+    </router-link>
+    <router-link class="nav-link" to="/artists">
+      <b-icon-star></b-icon-star>
+      <br />
+      Top Artists
+    </router-link>
+    <router-link class="nav-link" to="/tracks">
+      <b-icon-music-note-beamed></b-icon-music-note-beamed>
+      <br />
+      Top Tracks
+    </router-link>
+    <router-link class="nav-link" to="/recent">
+      <b-icon-clock-history></b-icon-clock-history>
+      <br />
+      Recent
+    </router-link>
+  </nav>
 </template>
-
-<style scoped>
-#nav {
-  position: fixed;
-  min-height: 100vh;
-  z-index: 2;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-#nav a {
-  padding: 20px;
-}
-</style>
