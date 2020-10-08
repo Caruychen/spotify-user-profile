fc395b6e0b164ba12898c5be386cdfaea87a00aa
Caruychen <caruychen@gmail.com>
2020-10-08 14:30:02 +0300

    update user profile interface

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -2,14 +2,42 @@
   <div id="user">
     <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" alt="loading" />
     <div v-else>
-      <b-img :src="user.images[0].url" fluid alt="Profile image"></b-img>
-      <h1>{{ user.display_name }}</h1>
+      <b-img
+        :src="user.images[0].url"
+        fluid
+        rounded
+        alt="Profile image"
+        id="user-profile-image"
+      ></b-img>
+      <b-jumbotron
+        id="user-profile-header"
+        :header="user.display_name"
+        header-level="4"
+      >
+        <b-container id="user-profile-summary">
+          <b-row align-h="center" cols-md="3">
+            <b-col>
+              playlists <br /><b>{{ playlistsTotal }}</b>
+            </b-col>
+            <b-col>
+              followed
+              <br />
+              <b>{{ followingTotal }}</b>
+            </b-col>
+            <b-col>
+              followers
+              <br />
+              <b>{{ user.followers.total }}</b>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-jumbotron>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   data() {
@@ -20,17 +48,33 @@ export default {
   computed: {
     ...mapState("profile", {
       user: state => state.user
+    }),
+    ...mapGetters({
+      playlistsTotal: "playlists/playlistsTotal",
+      followingTotal: "follow/followingTotal"
     })
   },
   methods: {
-    ...mapActions("profile", {
-      fetchUserProfile: "fetchUserProfile"
+    ...mapActions({
+      fetchUserProfile: "profile/fetchUserProfile",
+      fetchPlaylists: "playlists/fetchPlaylists",
+      fetchFollowing: "follow/fetchFollowing"
     })
   },
   created() {
     this.loading = true;
-    this.fetchUserProfile().then(() => (this.loading = false));
-    console.log(this.user);
+    (async () => {
+      try {
+        const profile = this.fetchUserProfile();
+        const playlists = this.fetchPlaylists();
+        const following = this.fetchFollowing();
+        await Promise.all([profile, playlists, following]);
+
+        this.loading = false;
+      } catch (error) {
+        console.log(`${error} - ${error.response.data.error.message}`);
+      }
+    })();
   }
 };
 </script>
