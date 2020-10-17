0d814d4df64bf6e1dfdd66e79456d14bbfabf916
Caruychen <caruychen@gmail.com>
2020-10-17 17:17:59 +0300

    move axios intercepter to module

--- client/src/main.js
+++ client/src/main.js
@@ -16,46 +16,6 @@ Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-axios.interceptors.request.use(
-  config => {
-    if (localStorage.access_token) {
-      config.headers = {
-        Authorization: "Bearer " + localStorage.access_token
-      };
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
-axios.interceptors.response.use(
-  response => {
-    return response;
-  },
-  async error => {
-    const status = error.response.status;
-    const originalConfig = error.config;
-
-    // Capture authentication error - return to login
-    if (status === 400) {
-      router.push({
-        name: "Login"
-      });
-    }
-    // Uses refresh token if access token invalid or expired
-    if (status === 401) {
-      const refreshStatus = await store.dispatch("auth/tokenRefresh");
-      if (refreshStatus === 200) {
-        originalConfig.headers.Authorization =
-          "Bearer " + localStorage.access_token;
-        return axios(originalConfig);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 Vue.config.productionTip = false;
 
 new Vue({
