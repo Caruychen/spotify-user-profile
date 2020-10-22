d83b0b9410e82d1650e4413d0297351158615953
Caruychen <caruychen@gmail.com>
2020-10-22 13:25:53 +0300

    add dashboard

---
+++ client/src/components/UserDashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="dashboard">
+    <h2>Your listening dashboard</h2>
+    <BaseTimeRangeSelector
+      version="button"
+      @updateTimeRange="onTimeRangeUpdate"
+    />
+  </div>
+</template>
+
+<script>
+import BaseTimeRangeSelector from "@/components/BaseTimeRangeSelector.vue";
+// import 'zingchart/es6';
+// import zingchartVue from 'zingchart-vue';
+
+export default {
+  data() {
+    return {
+      timeRange: "long_term"
+    };
+  },
+  components: {
+    BaseTimeRangeSelector
+  },
+  methods: {
+    onTimeRangeUpdate(timeRange) {
+      this.timeRange = timeRange;
+    }
+  }
+};
+</script>
