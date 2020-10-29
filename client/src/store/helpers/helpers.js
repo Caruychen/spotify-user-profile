de5c8e433d5f80f28dd5fb4c051ed24faab80bfd
Caruychen <caruychen@gmail.com>
2020-10-29 14:41:45 +0200

    add color interval splitter

--- client/src/store/helpers/helpers.js
+++ client/src/store/helpers/helpers.js
@@ -35,26 +35,35 @@ function filterEpisode(item) {
   };
 }
 
-function splitColors(colorArray, index, arrayLength) {
-  const groups = colorArray.length - 1;
+function colorInterval(colorRange, index, arrayLength) {
+  const groups = colorRange.length - 1;
   const groupMaxSize = Math.ceil(arrayLength / groups);
   const colorIndex = Math.floor(index / groupMaxSize);
-  const upperPortion = index < groupMaxSize ? index : index % (colorIndex * groupMaxSize);
+  const upperPortion =
+    index < groupMaxSize ? index : index % (colorIndex * groupMaxSize);
   const lowerPortion = groupMaxSize - upperPortion;
   const lower = [
-    parseInt(colorArray[colorIndex].substring(1, 3), 16) * lowerPortion,
-    parseInt(colorArray[colorIndex].substring(3, 5), 16) * lowerPortion,
-    parseInt(colorArray[colorIndex].substring(5), 16) * lowerPortion
-  ]
+    parseInt(colorRange[colorIndex].substring(1, 3), 16) * lowerPortion,
+    parseInt(colorRange[colorIndex].substring(3, 5), 16) * lowerPortion,
+    parseInt(colorRange[colorIndex].substring(5), 16) * lowerPortion
+  ];
   const upper = [
-    parseInt(colorArray[colorIndex+1].substring(1, 3), 16) * upperPortion,
-    parseInt(colorArray[colorIndex+1].substring(3, 5), 16) * upperPortion,
-    parseInt(colorArray[colorIndex+1].substring(5), 16) * upperPortion
-  ]
+    parseInt(colorRange[colorIndex + 1].substring(1, 3), 16) * upperPortion,
+    parseInt(colorRange[colorIndex + 1].substring(3, 5), 16) * upperPortion,
+    parseInt(colorRange[colorIndex + 1].substring(5), 16) * upperPortion
+  ];
   const result = lower.map((item, index) => {
-    return Math.round((item + upper[index]) / groupMaxSize).toString(16)
-  })
-  return `#${result.join("")}`
+    return Math.round((item + upper[index]) / groupMaxSize).toString(16);
+  });
+  return `#${result.join("")}`;
 }
 
-export { filterArtist, filterTrack, filterEpisode, splitColors };
+function colorArray(colorRange, arrayLength) {
+  const array = [];
+  for (let i = 0; i < arrayLength; i++) {
+    array.push(colorInterval(colorRange, i, arrayLength));
+  }
+  return array;
+}
+
+export { filterArtist, filterTrack, filterEpisode, colorInterval, colorArray };
