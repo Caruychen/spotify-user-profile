f17cf8a342297727baf95d98d60398b10e3859f1
Caruychen <caruychen@gmail.com>
2020-10-29 16:38:49 +0200

    adjust histogram chart

--- client/src/store/modules/chartconfigs.js
+++ client/src/store/modules/chartconfigs.js
@@ -14,28 +14,16 @@ export default {
     histogramData: {
       type: "bar",
       backgroundColor: "none",
-      plot: {
-        aspect: "histogram"
-      },
-      plotarea: {
-        margin: "dynamic"
-      },
-      "scale-y": {
-        label: {
-          text: "Frequency"
-        },
-        guide: {
-          alpha: 0.2
-        }
-      },
       tooltip: {
         text: "%v track(s)\n%k"
       }
     },
     pieColorRange: ["#6771E3", "#DD50A3", "#DDD150", "#50DD8A"],
     histogramColorRange: ["#6771E3", "#DD50A3"],
-    pieTitleHeight: 0,
-    pieDefaultHeight: 480
+    titleHeight: {
+      pie: 0,
+      histogram: 0
+    }
   },
   getters: {
     chartScale: (state, getters, rootState) => {
@@ -46,8 +34,14 @@ export default {
       }
       return 1;
     },
-    pieOffsetHeight: (state, getters) => {
-      return state.pieDefaultHeight * getters.chartScale - state.pieTitleHeight;
+    fontScale: (state, getters) => {
+      return getters.chartScale === 1
+        ? getters.chartScale
+        : getters.chartScale * 1.25;
+    },
+    offsetHeight: (state, getters) => type => {
+      const defaultChartHeight = 480;
+      return defaultChartHeight * getters.chartScale - state.titleHeight[type];
     },
     pieConfig: (state, getters, rootState, rootGetters) => timeRange => {
       const seriesData = rootGetters["features/valueCount"](
@@ -57,14 +51,10 @@ export default {
         const color = colorInterval(state.pieColorRange, index, array.length);
         return { ...item, backgroundColor: color };
       });
-      const fontScale =
-        getters.chartScale === 1
-          ? getters.chartScale
-          : getters.chartScale * 1.25;
       const valueBoxFontBase = 12;
       const sliceBase = 80;
       const tooltipFontBase = 16;
-      const tooltipYOffsetBase = getters.pieOffsetHeight / 2;
+      const tooltipYOffsetBase = getters.offsetHeight("pie") / 2;
       return {
         graphset: [
           {
@@ -72,7 +62,7 @@ export default {
             plot: {
               valueBox: {
                 text: "%t\n%npv%",
-                fontSize: valueBoxFontBase * fontScale,
+                fontSize: valueBoxFontBase * getters.fontScale,
                 decimals: 0,
                 placement: "out",
                 rules: [
@@ -97,7 +87,7 @@ export default {
             tooltip: {
               text:
                 '<span style="color:%color">%t:</span><br><span style="color:%color">%v tracks</span>',
-              fontSize: tooltipFontBase * fontScale,
+              fontSize: tooltipFontBase * getters.fontScale,
               backgroundColor: "none",
               borderColor: "none",
               anchor: "c",
@@ -120,29 +110,65 @@ export default {
       for (let i = 0; i < bins; i++) {
         values.push(i / bins);
       }
+      const fontSizeBase = 12;
+      const leftMarginBase = 38;
       return {
         graphset: [
           {
             ...state.histogramData,
             plot: {
+              aspect: "histogram",
               styles: colorArray(state.histogramColorRange, bins)
+            },
+            plotarea: {
+              marginTop: "dynamic",
+              marginBottom: "dynamic",
+              marginLeft:
+                leftMarginBase - (1 - getters.fontScale) * fontSizeBase,
+              marginRight: "10"
+            },
+            scaleX: {
+              values,
+              lineColor: "none",
+              tick: {
+                alpha: 0.5
+              },
+              item: {
+                fontSize: fontSizeBase * getters.fontScale
+              }
+            },
+            scaleY: {
+              label: {
+                text: "Tracks",
+                color: "#48ada3",
+                alpha: 0.8,
+                fontSize: fontSizeBase * getters.fontScale
+              },
+              item: {
+                fontSize: fontSizeBase * getters.fontScale
+              },
+              guide: {
+                alpha: 0.1,
+                lineStyle: "dashed"
+              },
+              lineColor: "none",
+              tick: {
+                visible: false
+              }
             },
             series: rootGetters["features/featureDist"](
               timeRange,
               bins,
               feature
-            ),
-            scaleX: {
-              values
-            }
+            )
           }
         ]
       };
     }
   },
   mutations: {
-    setPieTitleHeight(state, height) {
-      state.pieTitleHeight = height;
+    setTitleHeight(state, element) {
+      state.titleHeight[element.type] = element.height;
     }
   }
 };
