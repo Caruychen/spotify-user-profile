489152f8e06668b6de4bd67079764a33330ed5d5
Caruychen <caruychen@gmail.com>
2020-11-05 14:43:36 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
 
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+const querystring = require('query-string');
+const cookieParser = require('cookie-parser');
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8081/callback';
 let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:8080';
 const PORT = process.env.PORT || 8081;
 const scope = "user-read-private user-read-email playlist-read-private playlist-read-collaborative user-follow-read user-top-read user-read-recently-played user-read-playback-state user-modify-playback-state";
+const stateKey = "spotify_auth_state";
 
 if (process.env.NODE_ENV !== 'production') {
     REDIRECT_URI = 'http://localhost:8081/callback';
     FRONTEND_URI = 'http://localhost:8080';
 }
-
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const querystring = require('query-string');
-const cookieParser = require('cookie-parser');
-
 
 /**
  * Generates a random string containing numbers and letters
@@ -35,15 +36,20 @@ function generateRandomString(length) {
     }
     return text;
 }
-var stateKey = "spotify_auth_state";
 
 const app = express();
 
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '/public')));
+
 app
-    .use(express.static(__dirname + "/client/public"))
+    .use(express.static(path.resolve(__dirname + "/public")))
     .use(cors())
     .use(cookieParser());
 
+app.get('/', function (req, res) {
+    res.render(path.resolve(__dirname, '/public/index.html'));
+});
 
 app.get("/login", function (req, res) {
     var state = generateRandomString(16);
